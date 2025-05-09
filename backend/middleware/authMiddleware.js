1e905cce8bb1fd17e549c4e9e9068663a323013f
sumanbista <bistasuman17@gmail.com>
2025-05-09 19:26:38 -0400

    firebase key

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebaseServiceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
